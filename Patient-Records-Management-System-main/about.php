16d6a5e9a060972449877702f534e0d7753a0465
Shrishty620 <shrishtysingh1220@gmail.com>
2025-04-02 10:45:05 +0530

    first commit

---
+++ Patient-Records-Management-System-main/about.php
@@ -0,0 +1,82 @@
+<?php include("layout/navbar.php"); ?>
+
+
+<div class="container about-us banner">
+    <div class="row ">
+        <h1 class="my-md-5 py-5 text-center">ABOUT US</h1>
+    </div>
+</div>
+<div class="container">
+    <div class="row my-2">
+        <div class="col-md-2">
+            <h3>Who Are We?</h3>
+        </div>
+        <div class="col">
+            <p>A private clinical institution serving and affiliated to the Harare Institute of Technology. Our campus clinic was introduced in conjuction with the official opening of the institute in 1988. Since then it has grown from serving only the administration and students within the institute to the whole community around Belvedere, and other visiting residents in need of clinical care.</p>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="row my-2">
+        <div class="col-md-2">
+            <h3>Our Aims</h3>
+        </div>
+        <div class="col text-justify">
+            <p><span class="my-2 text-justify">To offer the best (safe, secure and high quality) on campus health care service in and around Zimbabwe.</span></p>
+            <p><span class="my-2">To support and develop community health and medical services.</span></p>
+            <p><span class="my-2">To enhance the functions of the Center for Clinical Research,foster a clinical research ethos, and promote clinical research and trials.</span></p>
+        </div>
+    </div>
+</div>
+<div class="container headings text-md-center my-2">
+    <h1>Our Team</h1>
+</div>
+<div class="container">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5">
+        <div class="col mb-4">
+            <div class="card">
+                <img src="img\tapiwa.JPG" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Tapiwa Motsi</h5>
+                    <p class="card-text">General Practitioner - MS in Biotechnology (China)</p>
+                </div>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="card">
+                <img src="img\lionel.JPG" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Lionel Edwards</h5>
+                    <p class="card-text">Optician - MS in Optometry (Canada)</p>
+                </div>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="card">
+                <img src="img\blessing.JPG" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Blessing Kashava</h5>
+                    <p class="card-text">Pharmacist - AD in Pharmaceutical Science (Malaysia)</p>
+                </div>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="card">
+                <img src="img\charity.JPG" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Charity Zimunya</h5>
+                    <p class="card-text">Nurse - BS in Health and Physical Fitness (Seychelles)</p>
+                </div>
+            </div>
+        </div>
+        <div class="col mb-4">
+            <div class="card">
+                <img src="img\dellon.JPG" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Dellon Gosha</h5>
+                    <p class="card-text">Dentist - PhD in Dentristy (Zimbabwe)</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include("layout/footer.php"); ?>
---
+++ Patient-Records-Management-System-main/index.php
@@ -0,0 +1,83 @@
+<?php include("layout/navbar.php"); ?>
+<div class="container home banner d-block d-md-none">
+    <div class="row ">
+        <h1 class="my-md-5 py-5 text-center">HIT Clinic</h1>
+    </div>
+</div>
+<div class="container">
+    <div class="row my-3 align-items-md-center d-none d-md-flex">
+        <div class="col-md">
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="img/home.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Hello There!</h5>
+                            <p>Help us help you get the treatment you need.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/home.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Looking to book an appointment?</h5>
+                            <p>Create an account with us today and have access to our practitioners 24/7.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/home.jpg" class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Want to find out more about us?</h5>
+                            <p>Visit the services tab and look at what we offer, or visit the about page for further insight.</p>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+        <div class="col-md text-md-center">
+            <h1>HIT Campus Clinic Online</h1>
+            <p>We're here for you! Assisting you in monitoring and having control over your health and wellbeing.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="container headings text-md-center my-2">
+            <h1>Vision</h1>
+        </div>
+        <div class="container-fluid">
+            <div class="row align-items-md-center">
+                <div class="col-md-3">
+                    <img src="img/doctor-1.jpg" class="img-fluid d-none d-md-block" alt="">
+                </div>
+                <div class="col-md-9">
+                    <p>To offer world standard campus clinical services.</p>
+                </div>
+                <div class="container-fluid headings text-md-center my-2">
+                    <h1>Mission Statement</h1>
+                </div>
+                <div class="container-fluid">
+                    <div class="row align-items-md-center">
+                        <div class="col-md-9">
+                            <p>To contribute to health and well-being by providing an exceptional service to all patients through integration of clinical practice and research.</p>
+                        </div>
+                        <div class="col-md-3">
+                            <img src="img/doctor-2.jpg" class="img-fluid d-none d-md-block" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include("layout/footer.php"); ?>
